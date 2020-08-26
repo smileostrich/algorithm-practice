8eb4e043044f0d24ef43b892429f8520d05ea3c5
Ian.IlMinMoon <ian.ilminmoon@gmail.com>
2020-08-26 22:09:11 +0900

    contents' update

--- basic_algorithm/graph/bfs.py
+++ basic_algorithm/graph/bfs.py
@@ -1,28 +1,44 @@
+def BFS(s, adj):
+    level = {s: 0}
+    parent = {s: None}
+    i = 1
+    frontier = [s]
+    while frontier:
+        next = []
+        for u in frontier:
+            for v in adj[u]:
+                if v not in level:
+                    level[v] = i
+                    parent[v] = u
+                    next.append(v)
+        frontier = next
+        i += 1
+
 # deque
-from collections import deque
-
-
-graph_list = {1: set([3, 4]),
-              2: set([3, 4, 5]),
-              3: set([1, 5]),
-              4: set([1]),
-              5: set([2, 6]),
-              6: set([3, 5])}
-root_node = 1
-
-def BFS_with_adj_list(graph, root):
-    visited = []
-    queue = deque([root])
-
-    while queue:
-        n = queue.popleft()
-        if n not in visited:
-            visited.append(n)
-            queue += graph[n] - set(visited)
-    return visited
-
-
-print(BFS_with_adj_list(graph_list, root_node))
+# from collections import deque
+#
+#
+# graph_list = {1: set([3, 4]),
+#               2: set([3, 4, 5]),
+#               3: set([1, 5]),
+#               4: set([1]),
+#               5: set([2, 6]),
+#               6: set([3, 5])}
+# root_node = 1
+#
+# def BFS_with_adj_list(graph, root):
+#     visited = []
+#     queue = deque([root])
+#
+#     while queue:
+#         n = queue.popleft()
+#         if n not in visited:
+#             visited.append(n)
+#             queue += graph[n] - set(visited)
+#     return visited
+#
+#
+# print(BFS_with_adj_list(graph_list, root_node))
 
 # queue
 # vertexList = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
