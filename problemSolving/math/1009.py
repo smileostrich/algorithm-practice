2f0e25eae685ace524c0a18df2a92cc6c6a286ff
Ian.IlMinMoon <ian.ilminmoon@gmail.com>
2020-10-23 22:57:05 +0900

    pb update

--- problemSolving/math/1009.py
+++ problemSolving/math/1009.py
@@ -1,14 +1,15 @@
 import sys
 import math
 
-dat = {1:[1], 2:[10, 2,4,6,8], 3:[1, 3,9,7], 4:[6,4], 5:[5], 6:[6], 7:[1,7,9,3], 8:[6,8,4,2], 9:[1,9], 0:[0]}
-print()
-T = int(sys.stdin.readline())
-for _ in range(T):
-    a, b = map(int, sys.stdin.readline().split())
-    num = int(str(a)[-1])
-    k = len(dat[num])
-    re = dat[num][b % k]
-    print(re)
+# dat = {1:[1], 2:[2,4,8,6], 3:[1, 3,9,7], 4:[6,4], 5:[5], 6:[6], 7:[1,7,9,3], 8:[6,8,4,2], 9:[1,9], 0:[0]}
+#
+# T = int(sys.stdin.readline())
+# for _ in range(T):
+#     a, b = map(int, sys.stdin.readline().split())
+#     num = int(str(a)[-1])
+#     k = len(dat[num])
+#     re = dat[num][b % k]
+#     print(re)
 
 
+# already sovled
