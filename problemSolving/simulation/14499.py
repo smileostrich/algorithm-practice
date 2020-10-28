82f01cefaac07cfbabd281f1a30c33895855719f
Ian.IlMinMoon <ian.ilminmoon@gmail.com>
2020-10-28 20:57:57 +0900

    pb update

---
+++ problemSolving/simulation/14499.py
@@ -0,0 +1,19 @@
+# 20.10.28
+
+from sys import stdin
+
+inp = lambda: map(int, stdin.readline().split())
+
+face_list = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+N, M, x, y, K = inp
+
+mat = [list(inp) for _ in range(N)]
+move_list = list(inp)
+
+for face in move_list:
+    if face == 1:
+
+    elif face == 2:
+    elif face == 3:
+    else:
