9061084a8caad1809f596c82ff2f6f5bfb145814
Ilmin Moon <ian.ilminmoon@gmail.com>
2020-11-07 23:35:50 +0900

    pb update

--- problemSolving/graph/7576_fail.py
+++ problemSolving/graph/7576_fail.py
@@ -1,37 +1,42 @@
+# 201106
+
+
+
 # 사실 풀긴했는데 3시간..... ㅠㅜㅜㅠㅠㅠㅠㅠ
 # bfs 알고리즘 구현 여러개로 해보기 + 자잘한 코딩실수로 인해서 시간 오래걸림
 # 내가짠거 + 인터넷 보고 리팩토링
-import sys
+# import sys
+#
 
-def bfs(queue):
-    k = 0
-    while queue:
-        next = []
-        for a, b in queue:
-            for dx, dy in near:
-                newX, newY = a+dx, b+dy
-                if (0 <= newY < N) and (0 <= newX < M) and (mat[newY][newX] == 0):
-                    next.append((newX, newY))
-                    mat[newY][newX] = 1
-        queue = next
-        k += 1
-
-    for i in mat:
-        if 0 in i:
-            return -1
-    return k-1
-
-
-M, N = map(int, sys.stdin.readline().split())
-mat = [[] for _ in range(N)]
-near = [(1, 0), (0, 1), (0, -1), (-1, 0)]
-queue = []
-for i in range(N):
-   mat[i] = list(map(int, sys.stdin.readline().split()))
-   for idxj, j in enumerate(mat[i]):
-       if j == 1:
-           queue.append((idxj,i))
-print(bfs(queue))
+# def bfs(queue):
+#     k = 0
+#     while queue:
+#         next = []
+#         for a, b in queue:
+#             for dx, dy in near:
+#                 newX, newY = a+dx, b+dy
+#                 if (0 <= newY < N) and (0 <= newX < M) and (mat[newY][newX] == 0):
+#                     next.append((newX, newY))
+#                     mat[newY][newX] = 1
+#         queue = next
+#         k += 1
+#
+#     for i in mat:
+#         if 0 in i:
+#             return -1
+#     return k-1
+#
+#
+# M, N = map(int, sys.stdin.readline().split())
+# mat = [[] for _ in range(N)]
+# near = [(1, 0), (0, 1), (0, -1), (-1, 0)]
+# queue = []
+# for i in range(N):
+#    mat[i] = list(map(int, sys.stdin.readline().split()))
+#    for idxj, j in enumerate(mat[i]):
+#        if j == 1:
+#            queue.append((idxj,i))
+# print(bfs(queue))
 
 
 
