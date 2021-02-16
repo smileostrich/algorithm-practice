19b474b49f1d9ed8ce63e713e397593cd7fea20c
ilminmoon <ian.ilminmoon@gmail.com>
2021-02-16 18:22:52 +0900

    pb swea 1210 update

--- problemSolving/SWEA/D4/1210.py
+++ problemSolving/SWEA/D4/1210.py
@@ -1,6 +1,27 @@
 for tc in range(1,11):
     t = input()
+    matrix = []
+    for i in range(99):
+        matrix.append(list(map(int, input().split())))
+    lastline = list(map(int, input().split()))
+    matrix.append(lastline)
 
-
-
-    print(f'{tc}')
+    cur = lastline.index(2)
+    for i in range(99,-1,-1):
+        if 0 <= cur - 1 < 100 and matrix[i][cur - 1] == 1:
+            cur -= 1
+            while True:
+                if 0<=cur-1<100 and matrix[i][cur-1] == 1:
+                    cur -= 1
+                else:
+                    break
+        elif 0<=cur+1<100 and matrix[i][cur+1] == 1:
+            cur += 1
+            while True:
+                if 0<=cur+1<100 and matrix[i][cur+1] == 1:
+                    cur += 1
+                else:
+                    break
+        else:
+            continue
+    print(f'#{tc} {cur}')
