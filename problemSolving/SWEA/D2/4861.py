0df406c99eddd31f505b15617b0f44a146b44d0b
ilminmoon <ian.ilminmoon@gmail.com>
2021-02-18 15:43:05 +0900

    swea 4861 update

---
+++ problemSolving/SWEA/D2/4861.py
@@ -0,0 +1,31 @@
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    li_mat = []
+    cond = False
+    for q in range(N):
+        tmp = input()
+        li_mat.append(tmp)
+        for i in range(N-M+2):
+            if cond == True:
+                break
+            for j in range(M // 2):
+                if tmp[i + j] != tmp[N - 1 - j]:
+                    break
+            else:
+                result = tmp[i:M+i]
+                cond = True
+    for k in range(N):
+        if cond == True:
+            break
+        for i in range(N-M+2):
+            for j in range(M // 2):
+                if li_mat[i + j][k] != li_mat[N - 1 - j][k]:
+                    break
+            else:
+                result = ''
+                for p in range(M):
+                    result += li_mat[i+p][k]
+                cond = True
+
+    print(f'#{tc} {result}')
--- problemSolving/SWEA/D3/1216.py
+++ problemSolving/SWEA/D3/1216.py
@@ -10,14 +10,47 @@ for _ in range(10):
         cur = 0
         while cur <= 98:
             cur_in = 1
-            while cur_in <= 99:
-                for i in range(cur_in//2):
-                    if cur+i != cur_in-i:
+            while cur <= cur_in and cur_in <= 99:
+                for j in range((cur_in-cur)//2):
+                    if tmp[cur+j] != tmp[cur_in-j]:
                         break
                 else:
-                    tmp_hi = cur_in-cur
+                    tmp_hi = cur_in-cur+1
                     if highest < tmp_hi:
                         highest = tmp_hi
                 cur_in += 1
             cur += 1
-    print(f'#{tc} ')
+    print(f'#{tc} {highest}')
+    # for i in range(100):
+    #     tmp = input()
+    #     li_mat.append(tmp)
+    #
+    #     cur = 0
+    #     while cur <= 98:
+    #         cur_in = 1
+    #         while cur < cur_in and cur_in <= 99:
+    #             for j in range((cur_in-cur)//2):
+    #                 if tmp[cur+j] != tmp[cur_in-j]:
+    #                     break
+    #             else:
+    #                 tmp_hi = cur_in-cur+1
+    #                 if highest < tmp_hi:
+    #                     highest = tmp_hi
+    #             cur_in += 1
+    #         cur += 1
+    # for k in range(100):
+    #     cur = 0
+    #     while cur <= 98:
+    #         cur_in = 1
+    #         while cur < cur_in and cur_in <= 99:
+    #             for j in range((cur_in - cur) // 2):
+    #                 if li_mat[cur + j][k] != li_mat[cur_in - j][k]:
+    #                     break
+    #             else:
+    #                 tmp_hi = cur_in - cur + 1
+    #                 if highest < tmp_hi:
+    #                     highest = tmp_hi
+    #             cur_in += 1
+    #         cur += 1
+
+    # print(f'#{tc} {highest}')
