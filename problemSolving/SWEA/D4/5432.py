58c052db4b8a842633c4bbd562184c73b64cdf5b
ilminmoon <ian.ilminmoon@gmail.com>
2021-02-19 21:05:31 +0900

    pb swea 5432

--- problemSolving/SWEA/D4/5432.py
+++ problemSolving/SWEA/D4/5432.py
@@ -1,3 +1,19 @@
 T = int(input())
 for tc in range(1, T+1):
-    
+    li_in = list(input())
+    stack = []
+    cnt = 0
+    cur = 0
+    for i in range(len(li_in)):
+        stack.append(li_in[i])
+        if li_in[i] == '(':
+            cnt += 1
+            cur += 1
+        else:
+            if stack[i-1] == '(':
+                cur -= 1
+                cnt -= 1
+                cnt += cur
+            else:
+                cur -= 1
+    print(f'{tc} {cnt}')
