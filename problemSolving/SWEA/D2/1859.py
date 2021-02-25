3112a4dcf12b40f957194bb919e32647ecb5b409
ilminmoon <ian.ilminmoon@gmail.com>
2021-02-25 09:42:41 +0900

    pb swea 1859 update

--- problemSolving/SWEA/D2/1859.py
+++ problemSolving/SWEA/D2/1859.py
@@ -1,27 +1,12 @@
-import heapq
-# T = int(input())
-
-for test_case in range(1, 1 + 1):
-    test = [3,4,5]
-    heapq.heapify(test)
-    for i in test:
-        print(heapq.heappop(test))
-# import heapq
-# T = int(input())
-#
-# for _ in range(1, T + 1):
-#     N = int(input())
-#     li_price = list(map(int, input().split()))
-#     # h_p = -1
-#     heapq.heapify(li_price)
-#     h_p = max(li_price)
-#     sorted_price = sorted(li_price, reverse=True)
-#     heapq.h
-#     for idx, price in enumerate(li_price):
-#         if price > h_p:
-#             h_p = price
-#
-#     for p in li_price:
-#         for c_p in sorted_price:
-#             if p >= cp:
-
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    li_pr = list(map(int, input().split()))
+    result = 0
+    max_val = 0
+    for i in range(N-1,-1,-1):
+        if li_pr[i] > max_val:
+            max_val = li_pr[i]
+        else:
+            result += max_val - li_pr[i]
+    print(f'#{tc} {result}')
---
+++ problemSolving/SWEA/D3/2805.py
@@ -0,0 +1,8 @@
+dx = [1,0]
+dy = [0,1]
+
+# T = int(input())
+T = 1
+for tc in range(1, T+1):
+    N = int(input())
+    matrix = [list(map(int, input().split())) for _ in range(N)]
