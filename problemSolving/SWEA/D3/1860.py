693d56c31f9d920327ab5fd0944494de76bad466
ilminmoon <ian.ilminmoon@gmail.com>
2021-02-25 17:01:11 +0900

    pb swea 1860 answer(not mine)

--- problemSolving/SWEA/D3/1860.py
+++ problemSolving/SWEA/D3/1860.py
@@ -1,29 +1,14 @@
-T = 1
-# T = int(input())
+# T = 1
+T = int(input())
 for tc in range(1,T+1):
-    # N, M, K = map(int, input().split())
-    # sec = list(map(int, input().split()))
-    N, M, K = 2,2,1
-    sec = [4,2]
+    N, M, K = map(int, input().split())
+    sec = list(map(int, input().split()))
+    # N, M, K = 2,2,1
+    # sec = [4,2]
     sec.sort()
-    mx = sec[-1]
-    cnt = 0
-
-    fk = [0]*(max(mx, M)+1)
-    for i in range(0,len(fk),M):
-        for j in range(i-1):
-            fk[i] += K
-            for p in range(1,M):
-                if i+p < len(fk):
-                    fk[i+p] += K
-    result = ''
-    for i in sec:
-        if fk[i] == 0:
-            result = "Impossible"
+    for i in range(N):
+        if sec[i] // M * K - i < 1:
+            print(f'#{tc} Impossible')
             break
-        else:
-            for j in range(i, len(fk)):
-                fk[j] -= 1
     else:
-        result = "Possible"
-    print(f'#{tc} {result}')
+        print(f'#{tc} Possible')
--- problemSolving/SWEA/D3/2805.py
+++ problemSolving/SWEA/D3/2805.py
@@ -6,3 +6,5 @@ T = 1
 for tc in range(1, T+1):
     N = int(input())
     matrix = [list(map(int, input().split())) for _ in range(N)]
+    for i in range(N//2):
+        for j in range()
