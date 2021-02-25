30e9d3cbee59cc2a04fd585c56ad9f406d35203e
ilminmoon <ian.ilminmoon@gmail.com>
2021-02-25 23:09:06 +0900

    pb swea 2805 update

--- problemSolving/SWEA/D3/2805.py
+++ problemSolving/SWEA/D3/2805.py
@@ -1,10 +1,13 @@
-dx = [1,0]
-dy = [0,1]
+T = int(input())
 
-# T = int(input())
-T = 1
 for tc in range(1, T+1):
     N = int(input())
-    matrix = [list(map(int, input().split())) for _ in range(N)]
-    for i in range(N//2):
-        for j in range()
+    matrix = [list(map(int, list(input()))) for _ in range(N)]
+    result = 0
+    for i in range(0,N//2+1):
+        for j in range(N//2-i, N//2+i+1):
+            result += matrix[i][j]
+    for i in range(1, N//2+1):
+        for j in range(i, N-i):
+            result += matrix[N//2+i][j]
+    print(f'#{tc} {result}')
