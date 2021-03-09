f84fa727bd581c32f0e633ad69617be12adc2c8d
ilminmoon <ian.ilminmoon@gmail.com>
2021-03-09 20:15:43 +0900

    pb programmer 타겟넘버 update

---
+++ problemSolving/programmers/dfsandbfs/타겟넘버_failed.py
@@ -0,0 +1,13 @@
+def solution(numbers, target):
+    q = [0]
+    for n in numbers:
+        s = []
+        for _ in range(len(q)):
+            x = q.pop()
+            s.append(x + n)
+            s.append(x + n*(-1))
+        q = s.copy()
+    return q.count(target)
+
+
+print(solution([1, 2, 3, 4, 5],5))
