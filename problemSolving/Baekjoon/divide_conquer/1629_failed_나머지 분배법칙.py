b986c0314a47174a0f2635f68104fc23def2abf4
ilminmoon <ian.ilminmoon@gmail.com>
2021-08-21 22:06:45 +0900

    boj 1629

---
+++ problemSolving/Baekjoon/divide_conquer/1629_failed_나머지 분배법칙.py
@@ -0,0 +1,11 @@
+A,B,C = map(int, input().split())
+
+def dac(a,b):
+    if b==1:
+        return a%C
+    temp = dac(a,b//2)
+    if b % 2 == 0:
+        return temp * temp % C
+    else:
+        return temp * temp * a % C
+print(dac(A,B))
