3c36d1d714848fc99724672d29c8681157eadfd8
ilminmoon <ian.ilminmoon@gmail.com>
2021-08-24 21:30:44 +0900

    boj 1541

--- problemSolving/Baekjoon/greedy/1541.py
+++ problemSolving/Baekjoon/greedy/1541.py
@@ -2,7 +2,6 @@ sen = input()
 num = []
 li_m = sen.split('-')
 result = sum(map(int, li_m[0].split('+')))
-if len(li_m) > 1:
-    for i in range(1,len(li_m)):
-        result -= sum(map(int, li_m[i].split('+')))
+for i in range(1,len(li_m)):
+    result -= sum(map(int, li_m[i].split('+')))
 print(result)
