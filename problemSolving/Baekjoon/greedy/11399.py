25057529450bcd96d4f1efc527d98d652bd0260a
ilminmoon <ian.ilminmoon@gmail.com>
2021-08-24 21:28:24 +0900

    boj 11399

--- problemSolving/Baekjoon/greedy/11399.py
+++ problemSolving/Baekjoon/greedy/11399.py
@@ -1,20 +1,32 @@
+# 두번째 풀이 greedy
+N = int(input())
+li_time = list(map(int, input().split()))
+li_time.sort()
+ans = 0
+s = 0
+for i in li_time:
+    s += i
+    ans += s
+print(ans)
+
+# 첫번째
 # 10분
 
-from sys import stdin
-import heapq
-
-
-inp = lambda: stdin.readline().rstrip()
-
-N = int(inp())
-li_draw = list(map(int, inp().split()))
-heapq.heapify(li_draw)
-li_sum = []
-
-for _ in range(N):
-    if li_sum:
-        li_sum.append(li_sum[-1] + heapq.heappop(li_draw))
-    else:
-        li_sum.append(heapq.heappop(li_draw))
-
-print(sum(li_sum))
+# from sys import stdin
+# import heapq
+#
+#
+# inp = lambda: stdin.readline().rstrip()
+#
+# N = int(inp())
+# li_draw = list(map(int, inp().split()))
+# heapq.heapify(li_draw)
+# li_sum = []
+#
+# for _ in range(N):
+#     if li_sum:
+#         li_sum.append(li_sum[-1] + heapq.heappop(li_draw))
+#     else:
+#         li_sum.append(heapq.heappop(li_draw))
+#
+# print(sum(li_sum))
